8e43ae244ecf0031e671fad3f57dfdcdc30ab7f2
Frede94 <32297979+Frede94@users.noreply.github.com>
2022-10-12 13:03:11 +0200

    SpecFlow

--- Buscompany.UnitTests/PriceCalculatorTests.cs
+++ Buscompany.UnitTests/PriceCalculatorTests.cs
@@ -10,16 +10,34 @@ namespace BusCompany.UnitTests
             calculator = new PriceCalculator();
         }
 
-        [Fact]
-        public void CalculatePrice_ValidInlineData_PriceIsCorrect()
+        [Theory]
+        [InlineData(BusType.Standard, 0, 2500)]
+        [InlineData(BusType.Mini, 99, 2594)]
+        [InlineData(BusType.Luxory, 100, 4747)]
+        [InlineData(BusType.Standard, 500, 6698)]
+        [InlineData(BusType.Standard, 501, 6704)]
+        //[InlineData(1,1, 22, -1)]
+        //[InlineData(1,1, 75, -1)]
+        public void CalculatePrice_ValidInlineData_PriceIsCorrect(BusType busType,int distance,decimal expectedPrice)
         {
             //Arrange
             PriceCalculator calculator = new PriceCalculator();
             //Act
-            decimal price = calculator.CalculatePrice(0, BusType.Standard);
+            decimal price = calculator.CalculatePrice(distance, busType);
             //Assert
-            Assert.Equal(2500, price);
+            Assert.Equal(price,expectedPrice);
         }
+
+        //[Fact]
+        //public void CalculatePrice_ValidInlineData_PriceIsWrong_Throws()
+        //{
+        //    //Arrange
+        //    PriceCalculator calculator = new PriceCalculator();
+        //    //Act
+        //    decimal price = calculator.CalculatePrice(distance, busType);
+        //    //Assert
+        //    Assert.Equal(price, expectedPrice);
+        //}
 
         [Fact]
         public void CalculatePrice_NegativeDistance_ThrowsException()
